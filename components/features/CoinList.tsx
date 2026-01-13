fc716543278574b4341d800a3dda79108a9bfa18
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-01-13 16:57:45 +0530

    stick the next and previous btn of coins table

--- components/features/CoinList.tsx
+++ components/features/CoinList.tsx
@@ -16,141 +16,138 @@ function CoinList() {
     const { coinList, fetchingCoinList } = useCoinList({ currentPageNumber });
 
     return (
-        <div className="coins-sst-container">
-            <table className="coins-server-side-table">
-                <colgroup>
-                    <col></col>
-                    <col className="w-[200px]"></col>
-                </colgroup>
+        <>
+            <div className="coins-sst-container">
+                <table className="coins-server-side-table">
+                    <thead>
+                        <tr>
+                            <th className="w-[5%] text-center">#</th>
+                            <th className="w-[35%] text-left">Coin</th>
+                            <th className="text-right">Current Price</th>
+                            <th className="text-right">1h</th>
+                            <th className="text-right">24h</th>
+                            <th className="text-right">Total Volume</th>
+                            <th className="text-right">Market Cap.</th>
+                            <th className="text-right">
+                                <div className="flex justify-end">
+                                    <div className="mr-[4px]">
+                                        Circulating Supply
+                                    </div>
 
-                <thead>
-                    <tr>
-                        <th className="w-[5%] text-center">#</th>
-                        <th className="w-[35%] text-left">Coin</th>
-                        <th className="text-right">Current Price</th>
-                        <th className="text-right">1h%</th>
-                        <th className="text-right">24h%</th>
-                        <th className="text-right">Total Volume</th>
-                        <th className="text-right">Market Cap.</th>
-                        <th className="text-right">
-                            <div className="flex justify-end">
-                                <div className="mr-[4px]">
-                                    Circulating Supply
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            <BsFillInfoCircleFill />
+                                        </TooltipTrigger>
+
+                                        <TooltipContent
+                                            data-side={'top'}
+                                            side={'top'}
+                                            className="w-[300px]"
+                                        >
+                                            The amount of coins that are circulating in the market and are in public
+                                            hands. It is analogous to the flowing shares in the stock market.
+                                        </TooltipContent>
+                                    </Tooltip>
                                 </div>
+                            </th>
+                        </tr>
+                    </thead>
 
-                                <Tooltip>
-                                    <TooltipTrigger>
-                                        <BsFillInfoCircleFill />
-                                    </TooltipTrigger>
+                    {
+                        fetchingCoinList ?
+                            <tbody>
+                                <tr>
+                                    <td colSpan={colspanValue} className="!p-[unset]">
+                                        <Skeleton className="rounded-md w-[100%] h-[490px]" />
+                                    </td>
+                                </tr>
+                            </tbody> :
+                            <tbody>
+                                {
+                                    ((coinList.length > 0) ? coinList.map((coin, index) => {
+                                        return (
+                                            <tr key={coin.id}>
+                                                <td className="text-center">
+                                                    {index + 1 + ((currentPageNumber > 1) ? ((currentPageNumber - 1) * 10) : 0)}
+                                                </td>
 
-                                    <TooltipContent
-                                        data-side={'top'}
-                                        side={'top'}
-                                        className="w-[300px]"
-                                    >
-                                        The amount of coins that are circulating in the market and are in public
-                                        hands. It is analogous to the flowing shares in the stock market.
-                                    </TooltipContent>
-                                </Tooltip>
-                            </div>
-                        </th>
-                    </tr>
-                </thead>
+                                                <td>
+                                                    <div className="min-w-[120px] flex items-center">
+                                                        <div className="min-w-[26px] mr-[8px]">
+                                                            {
+                                                                coin.image ?
+                                                                    <Image
+                                                                        className="object-cover rounded-[10px]"
+                                                                        width={24}
+                                                                        height={24}
+                                                                        alt={`Image of ${coin.name}`}
+                                                                        src={coin.image}
+                                                                    />
+                                                                    :
+                                                                    <div className="coin-letter-mark">
+                                                                        {coin.symbol[0].toUpperCase()}
+                                                                    </div>
+                                                            }
+                                                        </div>
 
-                {
-                    fetchingCoinList ?
-                        <tbody>
-                            <tr>
-                                <td colSpan={colspanValue} className="!p-[unset]">
-                                    <Skeleton className="rounded-md w-[100%] h-[490px]" />
-                                </td>
-                            </tr>
-                        </tbody> :
-                        <tbody>
-                            {
-                                ((coinList.length > 0) ? coinList.map((coin, index) => {
-                                    return (
-                                        <tr key={coin.id}>
-                                            <td className="text-center">
-                                                {index + 1 + ((currentPageNumber > 1) ? ((currentPageNumber - 1) * 10) : 0)}
-                                            </td>
+                                                        <div className="text-left font-semibold mr-[6px]">
+                                                            {coin.name}
 
-                                            <td>
-                                                <div className="min-w-[120px] flex items-center">
-                                                    <div className="min-w-[26px] mr-[8px]">
-                                                        {
-                                                            coin.image ?
-                                                                <Image
-                                                                    className="object-cover rounded-[10px]"
-                                                                    width={24}
-                                                                    height={24}
-                                                                    alt={`Image of ${coin.name}`}
-                                                                    src={coin.image}
-                                                                />
-                                                                :
-                                                                <div className="coin-letter-mark">
-                                                                    {coin.symbol[0].toUpperCase()}
-                                                                </div>
-                                                        }
-                                                    </div>
-
-                                                    <div className="text-left font-semibold mr-[6px]">
-                                                        {coin.name}
-
-                                                        <div className="text-[12px] text-gray-400">
-                                                            {coin.symbol.toUpperCase()}
+                                                            <div className="text-[12px] text-gray-400">
+                                                                {coin.symbol.toUpperCase()}
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </td>
+                                                </td>
 
-                                            <td className="text-right">
-                                                {formatValueIntoCommaSeparated(coin.current_price, 5, true)}
-                                            </td>
+                                                <td className="text-right">
+                                                    {formatValueIntoCommaSeparated(coin.current_price, 5, true)}
+                                                </td>
 
-                                            <td className="text-right">
-                                                {coin.price_change_percentage_1h_in_currency && <div className={`flex items-center justify-end ${(coin.price_change_percentage_1h_in_currency > 0 ? 'success-text' : 'danger-text')}`}>
-                                                    <span className="relative bottom-[1px]">
-                                                        {(coin.price_change_percentage_1h_in_currency > 0) ? <FaCaretUp /> : <FaCaretDown />}
-                                                    </span>
-                                                    {roundOffNumber(coin.price_change_percentage_1h_in_currency, 1) + '%'}
-                                                </div>}
-                                            </td>
+                                                <td className="text-right">
+                                                    {coin.price_change_percentage_1h_in_currency && <div className={`flex items-center justify-end ${(coin.price_change_percentage_1h_in_currency > 0 ? 'success-text' : 'danger-text')}`}>
+                                                        <span className="relative bottom-[1px]">
+                                                            {(coin.price_change_percentage_1h_in_currency > 0) ? <FaCaretUp /> : <FaCaretDown />}
+                                                        </span>
+                                                        {roundOffNumber(coin.price_change_percentage_1h_in_currency, 1) + '%'}
+                                                    </div>}
+                                                </td>
 
-                                            <td className="text-right">
-                                                {coin.price_change_percentage_24h && <div className={`flex items-center justify-end ${(coin.price_change_percentage_24h > 0 ? 'success-text' : 'danger-text')}`}>
-                                                    <span className="relative bottom-[1px]">
-                                                        {(coin.price_change_percentage_24h > 0) ? <FaCaretUp /> : <FaCaretDown />}
-                                                    </span>
-                                                    {roundOffNumber(coin.price_change_percentage_24h, 1) + '%'}
-                                                </div>}
-                                            </td>
+                                                <td className="text-right">
+                                                    {coin.price_change_percentage_24h && <div className={`flex items-center justify-end ${(coin.price_change_percentage_24h > 0 ? 'success-text' : 'danger-text')}`}>
+                                                        <span className="relative bottom-[1px]">
+                                                            {(coin.price_change_percentage_24h > 0) ? <FaCaretUp /> : <FaCaretDown />}
+                                                        </span>
+                                                        {roundOffNumber(coin.price_change_percentage_24h, 1) + '%'}
+                                                    </div>}
+                                                </td>
 
-                                            <td className="text-right">
-                                                {coin.total_volume && formatValueInCompactUsd(coin.total_volume, 2)}
-                                            </td>
+                                                <td className="text-right">
+                                                    {coin.total_volume && formatValueInCompactUsd(coin.total_volume, 2)}
+                                                </td>
 
-                                            <td className="text-right">
-                                                {coin.market_cap && formatValueInCompactUsd(coin.market_cap, 2)}
-                                            </td>
+                                                <td className="text-right">
+                                                    {coin.market_cap && formatValueInCompactUsd(coin.market_cap, 2)}
+                                                </td>
 
-                                            <td className="text-right">
-                                                {coin.circulating_supply && formatValueInCompactUsd(coin.circulating_supply, 2)}
-                                            </td>
-                                        </tr>
-                                    )
-                                }) : <tr>
-                                    <td
-                                        colSpan={colspanValue}
-                                        className="italic text-[#ccc] text-center"
-                                    >
-                                        No coins found.
-                                    </td>
-                                </tr>)
-                            }
-                        </tbody>
-                }
-            </table>
+                                                <td className="text-right">
+                                                    {coin.circulating_supply && formatValueInCompactUsd(coin.circulating_supply, 2)}
+                                                </td>
+                                            </tr>
+                                        )
+                                    }) : <tr>
+                                        <td
+                                            colSpan={colspanValue}
+                                            className="italic text-[#ccc] text-center"
+                                        >
+                                            No coins found.
+                                        </td>
+                                    </tr>)
+                                }
+                            </tbody>
+                    }
+                </table>
+            </div>
 
             <div className="pagination-btn-group space-x-2 mt-[12px] text-center">
                 <Button
@@ -171,7 +168,7 @@ function CoinList() {
                     Next
                 </Button>
             </div>
-        </div>
+        </>
     )
 }
 
